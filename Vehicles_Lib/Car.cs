5877005284099c0adcdc10f34c9f168f0b17b165
Bart Soete <soeteba@hotmail.com>
2018-05-17 10:30:45 +0200

    Klasse Toevoegen

---
+++ Vehicles_Lib/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles_Lib
+{
+    class Car
+    {
+        #region Globale variabelen
+        enum ColorCars {Groen, Rood,Geel,Zwart} // Definiëren Enumeratie
+
+        ColorCars colorCar; // Aanmaken globale variabele van type ColorCars
+        int actualSpeed;   // Aanmaken globale variabele van het type int
+
+        #endregion
+
+        #region Methoden
+        // Aanmaken van klassemethode KmNaarMijl (ontvangt 1 Parameter)
+
+        double KmNaarMijl(int speed)
+        {
+            return speed / 1.609344;
+        }
+        #endregion
+
+    }
+}
